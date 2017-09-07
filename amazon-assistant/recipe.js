9889958c2ba1abc58a9172dd6a53b99732a2daa7
Bianca Danforth <biancadanforth@gmail.com>
2017-09-07 15:17:00 -0700

    Initial notification bar prototype

---
+++ amazon-assistant/recipe.js
@@ -0,0 +1,16 @@
+// Shield study recipe
+const recipe = {
+  imgUrl: 'https://mobileapkworld.com/wp-content/uploads/2017/05/com.amazon.aa.png',
+  imgAltText: "Amazon Assistant logo",
+  message: 'Try the Amazon Assistant Add-on for Firefox!',
+  linkText: 'Learn more',
+  linkUrl: 'https://www.amazon.com/',
+  buttonAcceptLabel: 'Add to Firefox',
+  buttonRejectLabel: 'Not Now',
+  dropdownItemLabels: [
+    "Don't show me this again",
+    'Box',
+    'Other things',
+    'Harry Potter'
+  ],
+};
