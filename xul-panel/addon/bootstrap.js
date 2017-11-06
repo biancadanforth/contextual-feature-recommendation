c18785018b5dd789e27b121d736825bfe2b329e8
Bianca Danforth <biancadanforth@gmail.com>
2017-11-06 14:06:52 -0800

    Add addon icon to popup; remove unused files and code

--- xul-panel/addon/bootstrap.js
+++ xul-panel/addon/bootstrap.js
@@ -41,17 +41,12 @@ const WindowWatcher = {
       case "xul-window-registered":
         await this.inject(getDOMWindow(subject));
         break;
-      case "browser-delayed-startup-finished":
-      // This is for opening the popup immediately on startup in the case the addon is already installed before Firefox launches (i.e. unbranded builds where I can permanently install an unsigned addon)
-      // Per Matt N. if I am temporarily installing the addon on startup, this event may have already been fired before the addon is installed
-      // However having this event here covers the case when I am restarting Firefox on an unbranded build with the addon already installed
-      // TODO: Figure out why the event fires, but the popup only goes from "showing" to "removed"; it's never "shown"
-        showPopup(browserWindow);
     }
   },
 
   shutdown() {
     Services.obs.removeObserver(this, "xul-window-registered");
+    Services.obs.removeObserver(this, "browser-delayed-startup-finished");
 
     // Clean up injected content
     while (this.windowList.hasMoreElements()) {
@@ -65,7 +60,11 @@ const WindowWatcher = {
     popupContent.hidden = true;
     popupContent.id = `${this.popupID}-notification`;
     popupContent.innerHTML = `
-      <popupnotificationcontent class="custom-popup-example-content" orient="vertical">
+      <popupnotificationcontent
+        class="custom-popup-example-content"
+        orient="vertical"
+        style="margin:0"
+      >
         <browser
           id="custom-popup-example-browser"
           src="resource://custom-popup-example-addon-content/panel.html"
@@ -95,11 +94,6 @@ const WindowWatcher = {
     return popupSet;
   },
 
-  removeUnwantedDefaultPanelElements(domWindow) {
-    const eles = domWindow.document.getElementsByClassName(`${this.popupID}-icon`);
-    console.log(eles);
-  },
-
   async inject(domWindow) {
     if (domWindow.document.getElementById(this.popupID)) {
       throw new Error(`No such element by ID ${this.popupID} exists.`);
@@ -113,31 +107,18 @@ const WindowWatcher = {
     if (popupContent) {
       popupContent.remove();
     }
-    // TODO bdanforth: Check if I need to remove the domWindow "load" listener from getPopupSet method
+    // TODO bdanforth: Check if I need to remove the domWindow "load" listener from getPopupSet method (and how?)
   },
 };
 
 function install() {}
 
-async function startup(data, reason) {
+async function startup() {
   await WindowWatcher.startup();
 
   const browserWindow = RecentWindow.getMostRecentBrowserWindow();
 
-  const ADDON_INSTALL = 5;
-  const ADDON_STARTUP = 1;
-
-  // TODO: bdanforth - currently the addon works with or without the if statement
-  // using the reason code without a delay means the popup does not display on initial startup of Firefox
-  // ADDON_STARTUP occurs if you have the addon already installed and are opening Firefox (an unbranded build in the case of an unsigned addon)
-  // ADDON_INSTALL occurs on addon install!
-  if (reason === ADDON_INSTALL || reason === ADDON_STARTUP) {
-    browserWindow.setTimeout(() => showPopup(browserWindow), 500);
-  }
-
-  // showPopup(browserWindow);
-
-  // WindowWatcher.removeUnwantedDefaultPanelElements(browserWindow);
+  browserWindow.setTimeout(() => showPopup(browserWindow), 200);
 }
 
 function showPopup(browserWindow) {
@@ -150,7 +131,7 @@ function showPopup(browserWindow) {
       label: "Add to Firefox",
       accessKey: "A",
       callback: function() {
-        alert("You clicked 'Add to Firefox'.");
+        console.log("You clicked 'Add to Firefox'.");
       },
     },
     [
@@ -158,14 +139,14 @@ function showPopup(browserWindow) {
         label: "Not Now",
         accessKey: "N",
         callback: function() {
-          alert("You clicked 'Not Now'.");
+          console.log("You clicked 'Not Now'.");
         },
       },
       {
         label: "Never ask me again",
         accessKey: "e",
         callback: function() {
-          alert("You clicked 'Never ask me again'.");
+          console.log("You clicked 'Never ask me again'.");
         },
       },
     ],
@@ -173,11 +154,10 @@ function showPopup(browserWindow) {
       persistentWhileVisible: true,
       persistent: true,
       eventCallback: (state) => {
-        console.log(browserWindow.document);
         console.log(state);
       },
       hideClose: true,
-      popupIconClass: `${WindowWatcher.popupID}-icon`,
+      popupIconURL: `resource://custom-popup-example-addon-content/extensions-16.svg`,
     }
   );
 }
