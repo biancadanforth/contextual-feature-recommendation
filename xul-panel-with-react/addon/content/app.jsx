74ce5f5d4d34b6f4a01b07a1047647bc6ad7e241
Bianca Danforth <biancadanforth@gmail.com>
2017-11-10 09:48:43 -0800

    Using page scripts to render React app with message from JSM!
    Need now to pass a message from the page script to the JSM.

--- xul-panel-with-react/addon/content/app.jsx
+++ xul-panel-with-react/addon/content/app.jsx
@@ -7,62 +7,68 @@
 /* global React ReactDOM require content addMessageListener sendAsyncMessage */
 
 // TODO: Remove this and just use addMessageListener and sendAsyncMessage sans wrapper
-const self = {
-  port: {
-    on(header, handle) {
-      addMessageListener(header, {
-        receiveMessage(message) {
-          if (message.name === header)
-            handle(message.data);
-        },
-      });
-    },
-    emit(header, data) {
-      sendAsyncMessage(header, data);
-    },
-  },
-};
+// const self = {
+//   port: {
+//     on(header, handle) {
+//       addMessageListener(header, {
+//         receiveMessage(message) {
+//           if (message.name === header)
+//             handle(message.data);
+//         },
+//       });
+//     },
+//     emit(header, data) {
+//       sendAsyncMessage(header, data);
+//     },
+//   },
+// };
+
+function loadContent() {
+  console.log("successfully called this method from Feature.jsm");
+}
 
 const sanitizeHtml = (m) => { return m; }; // disabling the sanitization. not needed. only text from the code is sent.
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     console.log("constructor of App");
-//   }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor of App");
+  }
 
-//   render() {
-//     console.log('rendering');
-//     return (
-//       <div>
-//         Hi
-//       </div>
-//     );
-//   }
-// }
-
-function addCustomContent() {
-  const appEle = content.document.getElementById("app");
-  console.log(appEle);
-  appEle.innerHTML = "hello";
-  // ReactDOM.render(
-  //   React.createElement(App),
-  //   content.document.getElementById("app"),
-  // );
+  render() {
+    console.log('rendering');
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
 }
 
-self.port.on("FocusedCFR::load", (data) => {
-  content.addEventListener("load", () => {
-    const appEle = content.document.getElementById("app");
-    console.log(appEle);
-    addCustomContent();
-  });
+window.addCustomContent = function() {
+  console.log('hi');
+  ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("app"),
+  );
+}
+
+console.log('end of UI.js');
+
+// window.addEventListener("load", addCustomContent);
+
+// self.port.on("FocusedCFR::load", (data) => {
+  // content.addEventListener("load", () => {
+  //   const appEle = content.document.getElementById("app");
+  //   console.log(appEle);
+  //   addCustomContent();
+  // });
   // content.document.addEventListener("DOMContentLoaded", () => {
   //   console.log('DOMContentLoaded');
   //   const appEle = content.document.getElementById("app");
   //   console.log(appEle);
   //   addCustomContent();
   // });
-});
+// });
 
 // self.port.emit("panel-ready");
